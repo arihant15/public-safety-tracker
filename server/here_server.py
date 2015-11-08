1660edfccffc5e67424921c33febf7d78e54c9e7
Arihant <arihantraj@gmail.com>
2015-11-08 03:53:35 +0000

    parse xml

--- server/here_server.py
+++ server/here_server.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import sys
-
+import xml.etree.ElementTree
 
 pickup_location_1 = "35 W 33rd St, Chicago, IL 60616"
 pickup_location_2 = "3241 S Federal St, Chicago, IL 60616"
@@ -20,9 +20,19 @@ class PublicSafety():
         pad_gen = "&gen=9"
         pad_searchtext = "&searchtext="+self.home
         url = base_url + pad_app_id + pad_app_code + pad_gen + pad_searchtext
-        response = requests.get(url)
-        return response.content
-        #self.x_cordinate
-        #self.y_cordinate
-        
+        response = (requests.get(url)).content
+        e = xml.etree.ElementTree.fromstring(response)
+        position = e.findall("./Response/View/Result/Location/DisplayPosition")
+        for child in position:
+            self.latitude = child.find('Latitude').text
+            self.longitude = child.find('Longitude').text
+            print self.latitude
+            print self.longitude
 
+    def obtain_route(self):
+        base_url = "http://route.cit.api.here.com/routing/7.2/calculateroute.json"
+        pad_app_id = "?app_id="+self.properties['app-id']
+        pad_app_code = "&app_code="+self.properties['app-code']
+        self.car_location()
+        pad_waypoint0 = "&waypoint0=geo!"+self.latitude+","+self.longitude
+        pad_waypoint1 = "&waypoint1=geo!"
